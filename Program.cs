1f17be9be5b77b5f31c75673f77a7f6630728895
enotit <fkcman2003@gmail.com>
2020-05-22 00:28:15 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+namespace VK12TG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new VkApi();
+            
+            api.Authorize(new ApiAuthParams
+            {
+                ApplicationId = 123456,
+                Login = Users_information.login,
+                Password = Users_information.password,
+                Settings = Settings.All
+            });
+            Console.WriteLine(api.Token);
+            //var res = api.Groups.Get(new GroupsGetParams());
+
+            //Console.WriteLine(res.TotalCount);
+
+            Console.ReadLine();
+    }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# VK2TG
+ If you use only Telegram and have you friends in VK than it for you. 
